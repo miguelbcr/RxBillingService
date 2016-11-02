9f544d0e5fe1a80edfad3e937573613c27d10aea
miguelbcr <migueldebacor@gmail.com>
2016-11-02 09:06:07 +0100

    Bump gradle versions

--- rx_billing_service/src/test/java/com/miguelbcr/io/rx_billing_service/ExampleUnitTest.java
+++ rx_billing_service/src/test/java/com/miguelbcr/io/rx_billing_service/ExampleUnitTest.java
@@ -1,17 +1,52 @@
 package com.miguelbcr.io.rx_billing_service;
 
+import android.app.Activity;
+import com.miguelbcr.io.rx_billing_service.entities.ProductType;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subscribers.TestSubscriber;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
-  @Test
-  public void addition_isCorrect() throws Exception {
-    assertEquals(4, 2 + 2);
+  @Mock RxBillingService rxBillingService;
+  @Mock RxBillingServiceImpl rxBillingServiceImpl;
+  @Mock Activity activity;
+
+  @Before public void init() {
+    activity = new Activity();
+    rxBillingService = RxBillingService.getInstance(activity, false);
+  }
+
+  @Test public void Is_Billing_Supported() {
+    TestSubscriber<Boolean> ts = new TestSubscriber<>();
+    rxBillingService.isBillingSupported(ProductType.IN_APP).toFlowable().subscribe(ts);
+    ts.assertValue(true);
+  }
+
+  @Test public void Is_Billing_Supported_2() {
+    final AtomicReference<Boolean> v = new AtomicReference<>();
+    rxBillingService.isBillingSupported(ProductType.IN_APP)
+        .subscribe(new SingleObserver<Boolean>() {
+          @Override public void onSubscribe(Disposable d) {
+
+          }
+
+          @Override public void onSuccess(Boolean value) {
+            v.set(value);
+          }
+
+          @Override public void onError(Throwable e) {
+
+          }
+        });
+    assertEquals(true, v.get());
   }
 }
